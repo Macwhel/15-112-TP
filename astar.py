6457ce63e56be59eb723e098cb14010858a26bb8
Macwhel <60788935+Macwhel@users.noreply.github.com>
2021-11-14 00:10:00 -0500

    new commits

--- astar.py
+++ astar.py
@@ -1,4 +1,18 @@
+import queue
 
-def aStar(curPos, endPos, h) -> tuple:
+def h(curPos: tuple, endPos: tuple) -> int:
+    return (abs(curPos[0] - endPos[0]) + abs(curPos[1] - endPos[1]))
+
+# Referenced wikipedia for pseudocode
+# https://en.wikipedia.org/wiki/A*_search_algorithm#Pseudocode
+
+def aStar(curPos, endPos, gameMap) -> list:
     pass
+    
+
+
+# calls aStar to find the next position for the mob to move
+def getNextPos(curPos, endPos) -> tuple:
+    return aStar(curPos, endPos)[-2]
+
 
