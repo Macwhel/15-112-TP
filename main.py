8b01b76a7e834a9d0cc199c98b7a6b5761e9c654
Macwhel <imnotmaxwell@gmail.com>
2021-11-17 00:37:43 -0500

    worked on main

--- main.py
+++ main.py
@@ -5,17 +5,26 @@ from rectangle import *
 from mob import *
 from astar import *
 
+
 def appStarted(app):
+
     # these are things that'll change depending on difficulty
-    app.rows, app.cols = (20, 20)
-    numOfMobs = 2
+    app.rows, app.cols = (25, 25)
+    numOfMobs = 4
     app.timerDelay = 250
 
+    # make it relative to window size
     app.sW = min(app.width / app.rows, app.height / app.rows)
+
+    # initiliaze game states: 
+    '''Travel, Fight, etc.'''
+    app.Travel = False
+    app.mobFight = True
+    app.bossFight = False
 
     # initilalize map and then get start locations for player and end goal as
     # well as acceptable mob locations
-    '''Adjust maxTuns and maxLen scalings on difficulty'''
+    # Adjust maxTuns and maxLen scalings on difficulty****
     app.gameMap, app.pLoc, app.gLoc, mLocs = createMap(
         (app.rows, app.cols), int(1.2 * (app.rows + app.cols)), app.rows // 2.5)
     app.player = Player(app.pLoc[0], app.pLoc[1], app.sW / 3)
@@ -26,15 +35,6 @@ def appStarted(app):
     # create a list of mob classes
     app.mobList = [Mob(i[1], i[0], app.sW / 3, 10) for i in app.mobListLoc]
 
-
-
-    # for bugtesting
-    '''for i in app.gameMap:
-        print(i)'''
-
-    # initialize mobs in random locations
-
-# this is a little scuffed so change it later to be cleaner
 
 def keyPressed(app, event):
     lastCoords = (app.player.y, app.player.x)
@@ -50,81 +50,68 @@ def keyPressed(app, event):
             (mob.y, mob.x) = getNextPos((mob.y, mob.x), app.pLoc, app.gameMap)
             print(i, mob.y, mob.x, app.pLoc)'''
 
+    # change player location
     pY = app.player.getY()
     pX = app.player.getX()
-
-    # change player location
     if (pY not in range(app.rows) or pX not in range(app.cols) or 
         app.gameMap[pY][pX] == 0):
         app.player.setY(lastCoords[0])
         app.player.setX(lastCoords[1])
     
 def timerFired(app):
-
-    # change the coords of every mob thing
-    for mob in app.mobList:
-        (mob.y, mob.x) = getNextPos((mob.y, mob.x), app.pLoc, app.gameMap)
+    # change the coords of every mob
+    if app.Travel:
+        for mob in app.mobList:
+            (mob.y, mob.x) = getNextPos((mob.y, mob.x), (app.player.y, app.player.x), app.gameMap)
 
 def redrawAll(app, canvas):
-    for i in range(len(app.gameMap)):
-        for j in range(len(app.gameMap[0])):
-            cell = Rectangle(i * app.sW, j * app.sW, app.sW)
+    if app.Travel:
+        for i in range(len(app.gameMap)):
+            for j in range(len(app.gameMap[0])):
+                cell = Rectangle(i * app.sW, j * app.sW, app.sW)
 
-            if app.gameMap[i][j] == 0:
-                cell.setColor('black')
-            else:
-                if (i, j) == app.gLoc:
-                    cell.setColor('green')
-                else: 
-                    cell.setColor('white')
+                if app.gameMap[i][j] == 0:
+                    cell.setColor('black')
+                else:
+                    if (i, j) == app.gLoc:
+                        cell.setColor('green')
+                    else: 
+                        cell.setColor('white')
 
-            canvas.create_rectangle(cell.x, cell.y, cell.x2, cell.y2, fill = 
-                                    cell.color, width = 0)
+                canvas.create_rectangle(cell.x, cell.y, cell.x2, cell.y2, fill = 
+                                        cell.color, width = 0)
 
-            # coords for player
-            x = app.player.x * app.sW + (app.sW / 2)
-            y = app.player.y * app.sW + (app.sW / 2)
+        # coords for player
+        x = app.player.x * app.sW + (app.sW / 2)
+        y = app.player.y * app.sW + (app.sW / 2)
 
-            # draw player
+        # draw player
+        canvas.create_oval(
+            x - app.player.radius,
+            y - app.player.radius,
+            x + app.player.radius,
+            y + app.player.radius,
+            fill = app.player.color)
+
+        # draw mobs
+        for mob in app.mobList:
+            x = mob.x * app.sW + (app.sW / 2)
+            y = mob.y * app.sW + (app.sW / 2)
             canvas.create_oval(
-                x - app.player.radius,
-                y - app.player.radius,
-                x + app.player.radius,
-                y + app.player.radius,
-                fill = app.player.color)
-
-            # draw mobs
-            for mob in app.mobList:
-                x = mob.x * app.sW + (app.sW / 2)
-                y = mob.y * app.sW + (app.sW / 2)
-                canvas.create_oval(
-                    x - mob.rad,
-                    y - mob.rad,
-                    x + mob.rad,
-                    y + mob.rad,
-                    fill = "red"
-                )
-
-            # debugging purposes
-            '''x = app.mobList[0].x * app.sW + (app.sW / 2)
-            y = app.mobList[0].y * app.sW + (app.sW / 2)
-            canvas.create_oval(
-                x - app.mobList[0].rad,
-                y - app.mobList[0].rad,
-                x + app.mobList[0].rad,
-                y + app.mobList[0].rad,
+                x - mob.rad,
+                y - mob.rad,
+                x + mob.rad,
+                y + mob.rad,
                 fill = "red"
             )
 
-            x1 = app.mobList[1].x * app.sW + (app.sW / 2)
-            y1 = app.mobList[1].y * app.sW + (app.sW / 2)
-            canvas.create_oval(
-                x1 - app.mobList[1].rad,
-                y1 - app.mobList[1].rad,
-                x1 + app.mobList[1].rad,
-                y1 + app.mobList[1].rad,
-                fill = "blue"
-            )'''
+    elif app.mobFight:
+        canvas.create_rectangle(0, 0, app.width, app.height, fill = "black")
+
+        pass
+    
+    elif app.bossFight:
+        pass
 
 
 
