ce1ef08a26cb427eeaa889cd02096c04701c6dd4
Macwhel <60788935+Macwhel@users.noreply.github.com>
2021-11-24 01:42:48 -0500

    tried stuff out

--- test.py
+++ test.py
@@ -1,12 +1,24 @@
-rows = cols = 5
+import random
+
+rows = cols = 10
 
 def pprintBoard(board: list) -> None:
     for row in board:
         print(row)
 
+# init board
 board = [[0] * cols for _ in range(rows)]
 
+# init locations of all cells
+
+
 pprintBoard(board)
+
+# get a random cell
+# connect it with another cell
+
+r, c = random.randrange(rows), random.randrange(cols)
+
 
 
 
